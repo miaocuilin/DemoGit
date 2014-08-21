3a17618f7b3a040c39ffc391f7dde1bdb7c7991a
Rick_rui <jingrui.mao@gmail.com>
2014-08-21 09:41:08 +0800

    初始化

---
+++ DemoGit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoGit
+//
+//  Created by zhangjr on 14-8-21.
+//  Copyright (c) 2014年 自学OC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
